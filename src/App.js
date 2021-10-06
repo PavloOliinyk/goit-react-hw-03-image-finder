934c73f40642540d21393363e744f7249ba48e8b
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-10-06 18:03:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,73 @@
-import { Component } from 'react';
-import ImageGallery from './ImageGallery';
-import Searchbar from './Searchbar';
+import { Component } from "react";
+import Loader from "react-loader-spinner";
+import ImageGallery from "./ImageGallery";
+import Searchbar from "./Searchbar";
 // import Loader from "./Loader";
-import Button from './Button';
-import Modal from './Modal';
-import ImageApi from './api/imageApi';
-import s from './App.module.css';
+import Button from "./Button";
+import Modal from "./Modal";
+import ImageApi from "./api/imageApi";
+import s from "./App.module.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const image = new ImageApi();
 
 class App extends Component {
   state = {
+    query: "",
     images: [],
     showModal: false,
-    modalImage: '',
+    modalImage: "",
+    modalAltText: "",
+    showLoader: false
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state && this.state.images.length > 12) {
-      window.scrollTo({
-        top: document.documentElement.scrollHeight,
-        behavior: 'smooth',
+    if (prevState.query !== this.state.query) {
+      console.log(this.state.showLoader);
+      this.setState({ showLoader: true });
+      image.resetPage();
+      image.query = this.state.query;
+      image.fetchImageOrPhoto().then((images) => {
+        this.setState({ showLoader: true });
+        this.setState({ images });
       });
+      image.incrementpage();
+
+      // window.scrollTo({
+      //   top: document.documentElement.scrollHeight,
+      //   behavior: "smooth"
+      // });
     }
   }
 
-  modalToggle = e => {
-    console.log(e.target);
+  modalToggle = (e) => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
+      modalImage: !showModal ? e.target.dataset.src : "",
+      modalAltText: !showModal ? e.target.alt : ""
     }));
   };
 
-  handleSumbit = value => {
-    image.resetPage();
-    image.query = value;
-    image.fetchImageOrPhoto().then(images => this.setState({ images }));
-    image.incrementpage();
+  handleSumbit = (value) => {
+    // this.setState({ showLoader: true });
+    this.setState({ query: value });
+    // image.resetPage();
+    // image.query = value;
+    // image
+    //   .fetchImageOrPhoto()
+    //   .then((images) => this.setState({ images, showLoader: false }));
+    // image.incrementpage();
   };
 
   loadMoreImages = () => {
-    image.fetchImageOrPhoto().then(newImages => {
+    image.fetchImageOrPhoto().then((newImages) => {
       if (!newImages.length) {
         return;
       }
 
       this.setState(({ images }) => {
         return {
-          images: [...images, ...newImages],
+          images: [...images, ...newImages]
         };
       });
     });
@@ -63,16 +83,28 @@ class App extends Component {
             <ImageGallery
               images={this.state.images}
               onModalClick={this.modalToggle}
+              query={this.state.query}
+              image={image}
             />
             <Button onSearch={this.loadMoreImages} />
           </>
         )}
-        {/* <Loader /> */}
 
         {this.state.showModal && (
           <Modal
-            largeImg={this.state.modalImage}
+            modalImage={this.state.modalImage}
+            modalAltText={this.state.modalAltText}
             onModalClick={this.modalToggle}
+          />
+        )}
+        {this.state.showLoader && (
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+            style={{ textAlign: "center" }}
           />
         )}
       </div>
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,28 +1,34 @@
-import { Component } from 'react';
-import s from './Modal.module.css';
+import { Component } from "react";
+import s from "./Modal.module.css";
 
 class Modal extends Component {
   componentDidMount() {
-    window.addEventListener('keydown', this.closeModalByEsc);
+    window.addEventListener("keydown", this.closeModalByEsc);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModalByEsc);
+    window.removeEventListener("keydown", this.closeModalByEsc);
   }
 
-  closeModalByEsc = e => {
-    if (e.code !== 'Escape') {
+  closeModalByEsc = (e) => {
+    if (e.code !== "Escape") {
       return;
     }
 
     this.props.onModalClick();
   };
 
+  closeModalByBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      this.props.onModalClick();
+    }
+  };
+
   render() {
     return (
-      <div className={s.Overlay} onClick={this.props.onModalClick}>
+      <div className={s.Overlay} onClick={this.closeModalByBackdropClick}>
         <div className={s.Modal}>
-          <img src={this.props.largeImg} alt="" />
+          <img src={this.props.modalImage} alt={this.props.modalAltText} />
         </div>
       </div>
     );
