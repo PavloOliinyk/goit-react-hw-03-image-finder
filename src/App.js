a7c7587ff644aa24898e78c7d8ed6b46ee6e6586
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-10-06 23:46:52 +0300

    добавлен loader

--- src/App.js
+++ src/App.js
@@ -1,76 +1,75 @@
-import { Component } from "react";
-import Loader from "react-loader-spinner";
-import ImageGallery from "./ImageGallery";
-import Searchbar from "./Searchbar";
+import { Component } from 'react';
+import Loader from 'react-loader-spinner';
+import ImageGallery from './ImageGallery';
+import Searchbar from './Searchbar';
 // import Loader from "./Loader";
-import Button from "./Button";
-import Modal from "./Modal";
-import ImageApi from "./api/imageApi";
-import s from "./App.module.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Button from './Button';
+import Modal from './Modal';
+import ImageApi from './api/imageApi';
+import s from './App.module.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const image = new ImageApi();
 
 class App extends Component {
   state = {
-    query: "",
+    query: '',
     images: [],
     showModal: false,
-    modalImage: "",
-    modalAltText: "",
-    showLoader: false
+    modalImage: '',
+    modalAltText: '',
+    showLoader: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.query !== this.state.query) {
-      console.log(this.state.showLoader);
       this.setState({ showLoader: true });
       image.resetPage();
       image.query = this.state.query;
-      image.fetchImageOrPhoto().then((images) => {
-        this.setState({ showLoader: true });
-        this.setState({ images });
-      });
+      image
+        .fetchImageOrPhoto()
+        .then(images => this.setState({ images }))
+        .finally(() => this.setState({ showLoader: false }));
       image.incrementpage();
-
-      // window.scrollTo({
-      //   top: document.documentElement.scrollHeight,
-      //   behavior: "smooth"
-      // });
     }
   }
 
-  modalToggle = (e) => {
+  modalToggle = e => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
-      modalImage: !showModal ? e.target.dataset.src : "",
-      modalAltText: !showModal ? e.target.alt : ""
+      modalImage: !showModal ? e.target.dataset.src : '',
+      modalAltText: !showModal ? e.target.alt : '',
     }));
   };
 
-  handleSumbit = (value) => {
-    // this.setState({ showLoader: true });
-    this.setState({ query: value });
-    // image.resetPage();
-    // image.query = value;
-    // image
-    //   .fetchImageOrPhoto()
-    //   .then((images) => this.setState({ images, showLoader: false }));
-    // image.incrementpage();
+  handleSumbit = value => {
+    this.setState({ query: value, images: [] });
   };
 
   loadMoreImages = () => {
-    image.fetchImageOrPhoto().then((newImages) => {
-      if (!newImages.length) {
-        return;
-      }
+    this.setState({ showLoader: true });
+    image
+      .fetchImageOrPhoto()
+      .then(newImages => {
+        if (!newImages.length) {
+          console.log('конец');
+          return;
+        }
 
-      this.setState(({ images }) => {
-        return {
-          images: [...images, ...newImages]
-        };
+        this.setState(({ images }) => {
+          return {
+            images: [...images, ...newImages],
+          };
+        });
+      })
+      .finally(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+        this.setState({ showLoader: false });
       });
-    });
+
     image.incrementpage();
   };
 
@@ -78,24 +77,18 @@ class App extends Component {
     return (
       <div className={s.App}>
         <Searchbar onSubmit={this.handleSumbit} />
+
         {this.state.images.length > 1 && (
           <>
             <ImageGallery
               images={this.state.images}
               onModalClick={this.modalToggle}
               query={this.state.query}
-              image={image}
             />
-            <Button onSearch={this.loadMoreImages} />
+            {!this.state.showLoader && (
+              <Button onSearch={this.loadMoreImages} />
+            )}
           </>
-        )}
-
-        {this.state.showModal && (
-          <Modal
-            modalImage={this.state.modalImage}
-            modalAltText={this.state.modalAltText}
-            onModalClick={this.modalToggle}
-          />
         )}
         {this.state.showLoader && (
           <Loader
@@ -103,8 +96,16 @@ class App extends Component {
             color="#00BFFF"
             height={100}
             width={100}
-            timeout={3000} //3 secs
-            style={{ textAlign: "center" }}
+            timeout={50000} //3 secs
+            style={{ textAlign: 'center' }}
+          />
+        )}
+
+        {this.state.showModal && (
+          <Modal
+            modalImage={this.state.modalImage}
+            modalAltText={this.state.modalAltText}
+            onModalClick={this.modalToggle}
           />
         )}
       </div>
--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,10 +1,7 @@
-import { Component } from "react";
-import s from "./Button.module.css";
+import { Component } from 'react';
+import s from './Button.module.css';
 
 class Button extends Component {
-  state = {
-    disabled: true
-  };
   render() {
     return (
       <button type="button" className={s.Button} onClick={this.props.onSearch}>
